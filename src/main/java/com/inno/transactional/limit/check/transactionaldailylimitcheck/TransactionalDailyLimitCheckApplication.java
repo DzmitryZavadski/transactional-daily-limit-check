d369fd6597a5e933f91551035b7a414c03f628fc
DzmitryZavadski <dzmitry.zavadski@innowise.com>
2026-04-03 08:38:29 +0200

    first commit

---
+++ src/main/java/com/inno/transactional/limit/check/transactionaldailylimitcheck/TransactionalDailyLimitCheckApplication.java
@@ -0,0 +1,13 @@
+package com.inno.transactional.limit.check.transactionaldailylimitcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionalDailyLimitCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionalDailyLimitCheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inno/transactional/limit/check/transactionaldailylimitcheck/TransactionalDailyLimitCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inno.transactional.limit.check.transactionaldailylimitcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionalDailyLimitCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
